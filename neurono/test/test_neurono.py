703681e6c985626cfaf2fed9a2fe4aaa646c954c
Jason Rudy <jcrudy@gmail.com>
2013-06-30 13:30:57 -0700

    working on tests, removing build files

--- neurono/test/test_neurono.py
+++ neurono/test/test_neurono.py
@@ -23,6 +23,19 @@ class TestNeuralNetwork(object):
         net.fit(X,y)
         error = 0.5*((net.predict(X)[0,0] - y[0,0])**2)
         assert_true(error < thresh)
+    
+    def test_fit_harder(self):
+        X = numpy.random.normal(size=(100,10))
+        beta = numpy.random.normal(size=10)
+        y = (numpy.dot(X,beta)).reshape((100,1))
+        y = 1.0 / (1.0 + numpy.exp(-y))
+        print y
+        net = NeuralNetwork([10,50,1])
+        net.fit(X,y)
+        pred = net.predict(X)
+        error = numpy.mean(0.5*((X-pred)**2))
+        
+        
         
 if __name__ == '__main__':
     import nose

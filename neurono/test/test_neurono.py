995497502cbfedb69b32f5c1769e71d0f9335c90
Jason Rudy <jcrudy@gmail.com>
2013-06-30 13:35:17 -0700

    Removed all pyc files, improved test_fit

--- neurono/test/test_neurono.py
+++ neurono/test/test_neurono.py
@@ -16,26 +16,13 @@ class TestNeuralNetwork(object):
         assert_almost_equal(net.predict(X)[0,0],y[0,0])
         
     def test_fit(self):
-        X = numpy.array([[2.0]])
-        y = numpy.array(1.0 / (1.0 + numpy.exp(-1.0/(1.0+numpy.exp(-X)))))
+        X = numpy.array([[2.0, 3.0]])
+        y = numpy.array(1.0 / (1.0 + numpy.exp(-1.0/(1.0+numpy.exp(-numpy.dot(X,X.transpose()))))))
         thresh = .0000001
-        net = NeuralNetwork([1,2,1],thresh=thresh)
+        net = NeuralNetwork([2,4,1],thresh=thresh)
         net.fit(X,y)
         error = 0.5*((net.predict(X)[0,0] - y[0,0])**2)
         assert_true(error < thresh)
-    
-    def test_fit_harder(self):
-        X = numpy.random.normal(size=(100,10))
-        beta = numpy.random.normal(size=10)
-        y = (numpy.dot(X,beta)).reshape((100,1))
-        y = 1.0 / (1.0 + numpy.exp(-y))
-        print y
-        net = NeuralNetwork([10,50,1])
-        net.fit(X,y)
-        pred = net.predict(X)
-        error = numpy.mean(0.5*((X-pred)**2))
-        
-        
         
 if __name__ == '__main__':
     import nose
